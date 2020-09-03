bb684dbf24f2953bb20869d24597d84062ecd949
Ivan Kim M. Pasus <ivpasus@gmail.com>
2020-09-03 09:17:51 +0800

    to git

---
+++ WebApplication1/Model/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+	public class UserModel
+	{
+
+		public class userDetails
+		{
+			public string id { get; set; }
+			public string username { get; set; }
+
+		}
+	}
+}
